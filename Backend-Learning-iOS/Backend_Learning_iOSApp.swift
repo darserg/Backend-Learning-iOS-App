6b2495cfe1f8cd538dd9f7f228b2fbec53f3e0b6
Darserg <Darserg1212>
2024-03-26 18:46:09 +0400

    Initial Commit

---
+++ Backend-Learning-iOS/Backend_Learning_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Backend_Learning_iOSApp.swift
+//  Backend-Learning-iOS
+//
+//  Created by Сергей Дарьин on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Backend_Learning_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
